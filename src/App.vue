eef41c3512f111ea6ca86b78f7c4f38a4583563c
drashti-7span <drashti-7span@gmail.com>
2023-01-02 19:55:22 +0530

    For loop

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    
+    <Home date="Hello World" msg="Hello 2"/>
+    <About />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Home,
+    About
   }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
-        <h1>Hello from Hello componetes</h1>
+        <h1 v-if="show">Hello from Home Components</h1>
+        <!-- <h1 v-else>Hello from Home Components</h1 >  -->
+        <button v-on:click="display">Toggle Element</button>
+        <h2>{{date}}</h2> 
+        <h2>{{msg}}</h2> 
+        <button v-on:click="hello('hello call')">Call Hello Function on click</button> 
+        <button v-on:mouseover="hello('hello call')">Call Hello Function mousehover</button> 
     </div>
     
 </template>
-
 <script>
-    export default{
-        name:'Home'
+export default {
+    name: "Home-tut",
+    props:{
+        date:String,
+        msg:String
+    },
+    methods:{
+        hello(date){
+            alert(date)
+        },
+        display()
+        {
+            this.show=!this.show
+        }
+    },
+    data()
+    {
+        return {show:true}
     }
+};
 </script>
